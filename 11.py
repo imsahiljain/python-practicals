5ccbefefe84d04daee08bf2a038ce855ea007935
imsahiljain <emailofsahiljain@gmail.com>
2022-02-07 13:25:21 +0530

    remove comment

--- 11.py
+++ 11.py
@@ -7,15 +7,6 @@ while i < n:
     i += 1
 print("Created list of Integers is - ", L)
 
-# y = int(input("Enter number of elements you want to add in your list of strings - "))
-# i = 0
-# D = []
-# while i < y:
-#     z = input("Enter a string - ")
-#     D.append(z)
-#     i += 1
-# print("Created list of strings is - ", D)
-
 # 11 Print a new list after removing the 0th, 4th and 5th elements.
 L2 = []
 for x in range(len(L)):
